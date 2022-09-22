4c399b5acb9f160f90bfc3fe5ab7cab9a39149d7
the80hz <dnl.vlgv@mail.ru>
2022-09-22 12:40:37 +0400

    сделал определение текущей даты

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,6 +1,22 @@
 import os
-import requests
 from bs4 import BeautifulSoup
+import time
+import requests
+from datetime import datetime
 
-if __name__ == '__main__':
-    print('Hello world!')
+start = time.time()
+
+url_samara = 'https://www.gismeteo.ru/diary/4618/'
+
+
+today_year = datetime.now().timetuple().tm_year
+today_month = datetime.now().timetuple().tm_mon
+
+year = 2008
+month = 1
+
+#while month <= 2022:
+
+print("Scraping task finished")
+end = time.time()
+print(str(round(end - start, 2)) + 'sec')
