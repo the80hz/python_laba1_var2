0650f26835690fc487db31e96a3d33633ee6499f
the80hz <dnl.vlgv@mail.ru>
2022-09-22 13:11:05 +0400

    создание dataset.csv и реквест

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,22 +1,35 @@
 import os
-from bs4 import BeautifulSoup
 import time
 import requests
 from datetime import datetime
+from bs4 import BeautifulSoup
+
 
 start = time.time()
 
-url_samara = 'https://www.gismeteo.ru/diary/4618/'
+_url_samara_ = 'https://www.gismeteo.ru/diary/4618/'
 
-
-today_year = datetime.now().timetuple().tm_year
-today_month = datetime.now().timetuple().tm_mon
+_today_year_ = datetime.now().timetuple().tm_year
+_today_month_ = datetime.now().timetuple().tm_mon
 
 year = 2008
 month = 1
 
-#while month <= 2022:
+database = open('dataset.csv', 'w+')
 
-print("Scraping task finished")
+while year <= _today_year_:
+    while month <= 12:
+        if year == _today_year_ and month == _today_month_:
+            print('Reached the current date')
+            break
+        url = _url_samara_ + str(year) + '/' + str(month)
+        request_go = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+
+
+        month += 1
+    year += 1
+    month = 1
+
+print('Scraping task finished')
 end = time.time()
 print(str(round(end - start, 2)) + 'sec')
