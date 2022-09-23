e4841a8daa1a2aac83f33316216ff605260c0a09
the80hz <dnl.vlgv@mail.ru>
2022-09-23 13:17:03 +0400

    скоро придут большие изменения

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,4 +1,3 @@
-import time
 from datetime import datetime
 from bs4 import BeautifulSoup
 from lxml import etree
@@ -7,14 +6,14 @@ import requests
 
 start = time.time()
 
-headers = {'user-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0',
+HEADERS = {'user-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0',
            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
            }
 
-_url_samara_ = 'https://www.gismeteo.ru/diary/4618/'
+URL = 'https://www.gismeteo.ru/diary/4618/'
 
-_today_year_ = datetime.now().timetuple().tm_year
-_today_month_ = datetime.now().timetuple().tm_mon
+TODAY_YEAR = datetime.now().timetuple().tm_year
+TODAY_MONTH = datetime.now().timetuple().tm_mon
 
 year = 2008
 month = 1
@@ -23,27 +22,26 @@ database = open('dataset.csv', 'w+')
 database.write('File generated: ' + str(datetime.today()) + '\n')
 database.write('data,temperature,pressure,wind \n')
 
-while year <= _today_year_:
+while year <= TODAY_YEAR:
     while month <= 12:
-        if year == _today_year_ and month == _today_month_:
+        if year == TODAY_YEAR and month == TODAY_MONTH:
             print('Reached the current date')
             break
 
-        url = _url_samara_ + str(year) + '/' + str(month) + '/'
+        url = URL + str(year) + '/' + str(month) + '/'
         print(url)
-        page = requests.get(url, headers=headers)
+        page = requests.get(url, headers=HEADERS)
 
         writer = csv.writer(database)
 
-        soup = BeautifulSoup(page.text, "lxml")
+        soup = BeautifulSoup(page.text, "html")
 
-        dom = etree.HTML(str(soup))
-        res = dom.xpath('//*[@id="data_block"]/table')[0].findall('tr')
         data = list()
 
-        print(res)
-        for row in res:
-            data.append([c.text for c in row.getchildren()])
+        for row in soup:
+            data.append()
+            
+
             writer.writerow(data)
 
         month += 1
