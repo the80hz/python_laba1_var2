e583a89e0823f0728fe5910ea290738719531396
the80hz <dnl.vlgv@mail.ru>
2022-11-08 20:26:23 +0400

    пункт 3 бета

--- Lab2/3WeekSplit.py
+++ Lab2/3WeekSplit.py
@@ -15,24 +15,15 @@ def split_by_week_n(filename, n):
     with open(filename, 'r', encoding='utf8') as dataset:
         reader = csv.reader(dataset)
         rows = list(reader)
-        # delete 2 first rows
         rows = rows[2:]
         dataset.close()
 
     weeks = []
-    for row in rows:
-        weeks.append(datetime.strptime(row[0], '%Y-%m-%d'))
+    # every 7 rows append to weeks
 
-    weeks = list(set(weeks))
-    weeks.sort()
+    print(weeks)
 
-    for i in range(0, len(weeks), n):
-        with open(f'{weeks[i].strftime("%Y-%m-%d")}-{(weeks[i+n-1] + timedelta(days=7)).strftime("%Y-%m-%d")}.csv', 'w+', encoding='utf8') as week_n:
-            writer = csv.writer(week_n)
-            for row in rows:
-                if weeks[i] <= datetime.strptime(row[0], '%Y-%m-%d') <= weeks[i+n-1]:
-                    writer.writerow(row)
-            week_n.close()
+
 
 
 if __name__ == "__main__":
