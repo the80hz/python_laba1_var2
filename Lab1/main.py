fc83c94c41241361bf0188bc25ce3fa96dae242d
the80hz <dnl.vlgv@mail.ru>
2022-11-15 16:01:01 +0400

    final

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,61 +1,75 @@
-from datetime import datetime
 from bs4 import BeautifulSoup
 import csv
 import requests
-
-URL = 'https://www.gismeteo.ru/diary/4618'
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
-}
-
-start = datetime.now().timestamp()
-YEAR = datetime.now().timetuple().tm_year
-MONTH = datetime.now().timetuple().tm_mon
+from datetime import datetime
 
 
-def request(url):
-    return requests.get(url, headers=HEADERS)
-
-
-def write_table(html, file):
+def write_table(url_from, file, year, month):
+    """
+    Функция write_table(url_from, file, year, month) записывает определенную таблицу с html сайта в file.csv файл.
+    Параметр url_from - ссылка на сайт, с которого будет производиться скачивание.
+    Параметр file - файл, в который будет записана таблица.
+    Параметр year - год, с которого будет записана в file.
+    Параметр month - месяц, с которого будет записана в file.
+    """
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
+    }
+    html = requests.get(f'{url_from}/{year}/{month}', headers=headers)
     soup = BeautifulSoup(html.text, 'html.parser')
 
-    writer = csv.writer(dataset)
+    writer = csv.writer(file)
     for row in soup.select('div[id="data_block"]>table>tbody:last-child>tr'):
         children = list(row.select('td'))
 
-        num = ([c.text for c in children[0]])
-        y = ''.join(num)
+        year = int(year)
+        month = int(month)
+        day = int("".join([c.text for c in children[0]]))
+        date = datetime(year, month, day)
 
-        date = [f'{year}.{month}.{str(y)}']
-        line = date + [c.text for c in children[1:3] + [children[5]]]
+        line = [date.strftime('%Y-%m-%d')] + [c.text for c in children[1:3] + [children[5]]]
 
         writer.writerow(line)
 
 
-if __name__ == "__main__":
-    with open('dataset.csv', 'w+', encoding='utf8') as dataset:
+def table_to_csv(filename, url_from, year, month):
+    """
+    Функция table_to_csv(filename, url_from, year, month) создает таблицу с именем
+    filename, записывает в нее таблицу из url, с даты указанной даты year.month.
+    Параметр filename - имя файла, в который будет записана таблица.
+    Параметр url_from - ссылка на сайт, с которого будет производиться скачивание.
+    Параметр year - год, с которого будет записана в file.
+    Параметр month - месяц, с которого будет записана в file.
+    """
+    with open(filename, 'w+', encoding='utf8') as dataset:
         print(f'File generated: {datetime.today()}', file=dataset)
         print('data,temperature,pressure,wind', file=dataset)
-        year = 2008
-        month = 1
 
-        while year <= YEAR:
+        today_year = datetime.now().timetuple().tm_year
+        today_month = datetime.now().timetuple().tm_mon
+
+        while year <= today_year:
             while month <= 12:
-                if year == YEAR and month == MONTH:
+                if year == today_year and month == today_month:
                     print('Reached the current date')
                     break
-
-                page = request(f'{URL}/{year}/{month}')
-                write_table(page, dataset)
+                write_table(url_from, dataset, year, month)
 
                 month += 1
             year += 1
             month = 1
 
-        dataset.close()
+
+if __name__ == "__main__":
+    start = datetime.now().timestamp()
+
+    url = 'https://www.gismeteo.ru/diary/4618'
+
+    min_year = 2008
+    min_month = 1
+
+    table_to_csv('../data/dataset.csv', url, min_year, min_month)
 
     end = datetime.now().timestamp()
     print(f'Scraping task finished in {round(end - start, 2)} sec')
-
