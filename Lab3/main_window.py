8948aa900a131428500894a15c95addad0843c84
the80hz <dnl.vlgv@mail.ru>
2022-11-26 17:29:54 +0400

    два окна ввода данных: сколько годов и месяцев

--- Lab3/main_window.py
+++ Lab3/main_window.py
@@ -6,7 +6,12 @@ from datetime import datetime
 
 from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox, QInputDialog, QTableWidget, \
     QTableWidgetItem, QHeaderView, QAbstractItemView, QMenu, QAbstractScrollArea, QAbstractItemView, \
-    QPushButton, QComboBox, QCheckBox, QSpinBox, QDoubleSpinBox, QLabel, QGroupBox, QGridLayout, QFormLayout
+    QPushButton, QComboBox, QCheckBox, QSpinBox, QDoubleSpinBox, QLabel, QGroupBox, QGridLayout, QFormLayout \
+    , QRadioButton, QButtonGroup, QSizePolicy, QScrollArea, QFrame, QTabWidget, QStackedWidget, QStackedLayout, \
+    QStatusBar, QProgressBar, QToolButton, QToolBar, QStyle, QStyleOption, QStylePainter, QStyleFactory, \
+    QStyleOptionProgressBar, QStyleOptionSlider, QStyleOptionToolButton, QStyleOptionButton, QStyleOptionTab, \
+    QStyleOptionTabWidgetFrame, QStyleOptionTabBarBase, QLineEdit, QPlainTextEdit, QDateEdit, QTimeEdit
+
 from PyQt6.QtGui import QCursor
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPixmap
@@ -18,6 +23,8 @@ from Lab2 import return_4
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.weeks = None
+        self.years = None
         self.button_annots = None
         self.button_dir = None
         # self.path_to_dataset = 'data/dataset.csv'
@@ -34,7 +41,7 @@ class MainWindow(QMainWindow):
             self.path_to_dataset = QFileDialog.getOpenFileName(self, 'Выберите файл с данными', '',
                                                                'CSV files (*.csv)')[0]
 
-        self.button_dir = QPushButton('Выбрать директорию', self)
+        self.button_dir = QPushButton('Выбрать директорию \nдля аннотаций', self)
         self.button_dir.move(20, 20)
         self.button_dir.resize(200, 50)
         self.button_dir.clicked.connect(self.open_dir)
@@ -48,6 +55,19 @@ class MainWindow(QMainWindow):
         self.button_annots.clicked.connect(self.create_annots)
         self.button_annots.show()
 
+        # create qlineedit
+        self.years = QLineEdit(self)
+        self.years.move(20, 120)
+        self.years.resize(200, 20)
+        self.years.show()
+
+        self.weeks = QLineEdit(self)
+        self.weeks.move(20, 150)
+        self.weeks.resize(200, 20)
+        self.weeks.show()
+
+
+
     def create_annots(self):
         with open(self.path_to_dataset, 'r', encoding='utf8') as dataset:
             reader = csv.reader(dataset)
@@ -59,7 +79,7 @@ class MainWindow(QMainWindow):
                 writer.writerows(date_data)
 
         self.button_annots.setText('Файл аннотации создан')
-        self.button_annots.setEnabled(False)
+        # self.button_annots.setEnabled(False)
 
     def open_dir(self):
         self.path_to_annots = QFileDialog.getExistingDirectory(self, 'Выберите директорию', '')[0]
