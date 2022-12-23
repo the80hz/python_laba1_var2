54aee8d0f06a4771b326dceec0ce271f7757fcb2
the80hz <dnl.vlgv@mail.ru>
2022-12-23 18:06:00 +0400

    прописал fillna

--- Lab4/run.py
+++ Lab4/run.py
@@ -4,7 +4,16 @@ import pandas as pd
 def dataframe(path: str) -> pd:
     _df = pd.read_csv(path, sep=",")
     _df["Date"] = pd.to_datetime(_df["Date"], format="%Y-%m-%d")
+    _df.fillna(
+        {
+            "Temperature": _df["Temperature"].mean(),
+            "Pressure": "no data",
+            "Wind": "no data",
+        },
+        inplace=True,
+    )
 
+    _df["Fahrenheit"] = _df["Temperature"] * 1.8 + 32
     return _df
 
 
